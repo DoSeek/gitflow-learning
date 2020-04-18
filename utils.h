206c4d0d5e601f08c5d1a7181250cfc845520409
doseek <temparymail@qq.com>
2020-04-18 10:53:26 +0800

    update: utils新增工具函数。

--- utils.h
+++ utils.h
@@ -3,7 +3,16 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
 int getArgsCount();
 void hello();
+string fileName();
+string filePath();
+string fileAbsPath();
 
 #endif // __UTILS_H__
